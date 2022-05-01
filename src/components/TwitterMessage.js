3929df6e77401e0c31570fb2b281e9996776ab0c
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-05-01 11:53:52 -0400

    commit tree 5c2862a5d5e086cc79ff80ffe267f9ad06886b81

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,41 @@
 import React from "react";
-
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
+  }
+
+  saveMessage = (event) => {
+    this.setState({
+      message: event.target.value,
+    })
+  }
+
+  charsRemaining = () => {
+    return this.props.maxChars - this.state.message.length
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={(event) => this.saveMessage(event)} />
+        <div>{this.charsRemaining()}</div>
       </div>
     );
   }
-}
 
+    
+          
+            
+    
+
+          
+    
+    
+  
+}
 export default TwitterMessage;
